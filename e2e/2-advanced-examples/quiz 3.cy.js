85f61d6e0361c33479692a658952cbe04a4779ff
ajengpsalsabila <92075012+ajengpsalsabila@users.noreply.github.com>
2024-11-26 21:39:34 +0700

    Update quiz 3.cy.js

--- e2e/2-advanced-examples/quiz 3.cy.js
+++ e2e/2-advanced-examples/quiz 3.cy.js
@@ -4,29 +4,29 @@ describe('Login Functionality - OrangeHRM', () => {
     // Test Case 1: Login dengan username dan password valid
     it('TC_001: Pengguna login dengan username dan password benar', () => {
       cy.visit(baseUrl);
-      cy.get('[name="username"]').type('Admin'); // Ganti dengan username yang valid
-      cy.get('[name="password"]').type('admin123'); // Ganti dengan password yang valid
+      cy.get('[name="username"]').type('Admin'); 
+      cy.get('[name="password"]').type('admin123'); 
       cy.get('button[type="submit"]').click();
   
-      // Verifikasi hasil
-      cy.url().should('include', '/dashboard'); // Pastikan redirect ke halaman dashboard
+      
+      cy.url().should('include', '/dashboard'); 
     });
   
     // Test Case 2: Login dengan password salah
     it('TC_002: Pengguna login dengan password salah', () => {
       cy.visit(baseUrl);
       cy.get('[name="username"]').type('Admin');
-      cy.get('[name="password"]').type('salah123'); // Password salah
+      cy.get('[name="password"]').type('salah123'); 
       cy.get('button[type="submit"]').click();
   
       // Verifikasi hasil
-      cy.contains('Invalid credentials').should('be.visible'); // Pesan error muncul
+      cy.contains('Invalid credentials').should('be.visible'); 
     });
   
     // Test Case 3: Login dengan username yang tidak terdaftar
     it('TC_003: Pengguna login dengan username yang tidak terdaftar', () => {
       cy.visit(baseUrl);
-      cy.get('[name="username"]').type('fakeuser'); // Username tidak terdaftar
+      cy.get('[name="username"]').type('fakeuser'); 
       cy.get('[name="password"]').type('admin123');
       cy.get('button[type="submit"]').click();
   
@@ -40,13 +40,13 @@ describe('Login Functionality - OrangeHRM', () => {
       cy.get('button[type="submit"]').click();
   
       // Verifikasi hasil
-      cy.contains('Required').should('be.visible'); // Pesan error untuk username dan password
+      cy.contains('Required').should('be.visible'); 
     });
   
     // Test Case 5: Tidak mengisi username saja
     it('TC_005: Pengguna tidak memasukkan username', () => {
       cy.visit(baseUrl);
-      cy.get('[name="password"]').type('admin123'); // Isi hanya password
+      cy.get('[name="password"]').type('admin123'); 
       cy.get('button[type="submit"]').click();
   
       // Verifikasi hasil
